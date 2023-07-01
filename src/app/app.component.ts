cb5475486a50454f887c2f7c9abc1d08999fe56b
Vladislav <fvd.18@uni-dubna.ru>
2023-07-01 12:02:06 +0500

    viewchild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,36 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import {ChildComponent3} from './templateVariables/child.component';
 
 @Component({
     selector: 'my-app',
     template:  `
-            <child-comp3 #counter></child-comp3>
-            <button (click)="counter.increment()">+</button>
-            <button (click)="counter.decrement()">-</button>
+             <child-comp3 #counter></child-comp3>
+             <button (click)="increment()">+</button>
+             <button (click)="decrement()">-</button>
+            <p #nameText>{{name}}</p>
+            <p>{{nameText.textContent}}</p>
+            <button (click)="change()">Изменить</button>
     `
 })
 
 export class AppComponent {
+    @ViewChild("nameText", {static:false})
+    nameParagraph: ElementRef|undefined;
 
+    name: string = "Tom";
+
+    change() {       
+        if(this.nameParagraph!== undefined) {
+            console.log(this.nameParagraph.nativeElement.textContent)
+            this.nameParagraph.nativeElement.textContent = "hell";
+        }
+    }
+
+
+    @ViewChild(ChildComponent3, {static: false})
+    private counterComponent: ChildComponent3|undefined;
+
+    increment() {this.counterComponent?.increment();}
+    decrement() {this.counterComponent?.decrement();}
 }
     
